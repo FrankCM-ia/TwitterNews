950513705270c6130ed3cd4f2e8d6d0bd4779536
FrankCM-ia <59652945+FrankCM-ia@users.noreply.github.com>
2021-08-01 12:25:03 -0500

    Update ExtraerTweets

--- ExtraerTweets.py
+++ ExtraerTweets.py
@@ -8,6 +8,7 @@ from autenticate import get_auth
 auth = get_auth()
 api = tweepy.API(auth, wait_on_rate_limit_notify=True , wait_on_rate_limit=True)
 
+
 #Encontrar tweets que son tendencia en el momento actual PERU = 23424919 , delimitador de peru 
 trends1 = api.trends_place(23424919)
 data = trends1[0]
@@ -20,7 +21,7 @@ names = list(set([trend["name"] for trend in trends]))
 
 # top 5
 #names = names[:10]
-print("Total de Temas"+ len(names))
+print("Total de Temas", len(names))
 print(names)
 
 a = input("Escoge un Tema: ")
@@ -28,22 +29,25 @@ names = [a]
 print(names)
 
 ntweets = int(input("Cuantos tweets deseas? => "))
+
 for key in names:
     # separamos temas por carpetas
     carpeta = 'tweets/' + str(key)
     graficos = 'Graficas/' + str(key)
     if not(os.path.isdir(carpeta)):
         os.mkdir(carpeta)
-        os.mkdir(graficos)
+        #os.mkdir(graficos)
 
- 
     for tweet in tweepy.Cursor(api.search, q = key, tweet_mode = "extended",  lang = 'es' ).items(ntweets):
-        #print(tweet._json)
-        name_file = carpeta + '/' + tweet._json["id_str"] + ".json"
-        #print("============================================================================")
-        with open(name_file, 'a') as file:
-            json.dump(tweet._json, file, indent=4)
-
+        text = tweet.full_text
+        if tweet.full_text.startswith('RT'):
+            rt = tweet.retweeted_status
+            text = rt.full_text
+            
+        name_file = carpeta + '/' + tweet.id_str + ".txt"
+        with open(name_file, 'a', encoding="utf-8") as file:
+            file.write(text)
+        
 
 
 
